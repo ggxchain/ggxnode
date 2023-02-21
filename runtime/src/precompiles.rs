3086904fde39aa264bc70111289aaf0336f1778a
akorchyn <artur.yurii.korchynskyi@gmail.com>
2023-02-21 13:58:08 +0200

    Fixed PR notes

--- runtime/src/precompiles.rs
+++ runtime/src/precompiles.rs
@@ -26,18 +26,36 @@ pub mod consts {
 	pub const BN128_MUL: H160 = hash(7);
 	pub const BN128_PAIRING: H160 = hash(8);
 	pub const BLAKE2F: H160 = hash(9);
+
+	/// F3 is also used in Celo, so preserve the address for contracts interoperability.
+	pub const ED25519_VERIFY: H160 = hash(0xF3);
 	pub const SHA3_FIPS256: H160 = hash(0x400);
 	/// 402 is used in Astar and Moonbeam, so preserve the address for contracts interoperability
 	pub const EC_RECOVER_PUBLIC_KEY: H160 = hash(0x402);
-	/// 403 is also used in Astar, so preserve the address for contracts interoperability.
-	/// TO DECIDE: Celo uses 0xF3 for it, so we need to decide which one to use.
-	pub const ED25519_VERIFY: H160 = hash(0x403);
 	/// 5002 is used in Astar, so preserve the address for contracts interoperability
 	pub const SR25519_VERIFY: H160 = hash(0x5002);
 	/// 5003 is used in Astar, so preserve the address for contracts interoperability
 	pub const ECDSA_VERIFY: H160 = hash(0x5003);
 	/// 5005 is used in Astar, so preserve the address for contracts interoperability
 	pub const XVM: H160 = hash(0x5005);
+
+	pub const SUPPORTED_PRECOMPILES: [H160; 15] = [
+		EC_RECOVER,
+		SHA256,
+		RIPEMD160,
+		IDENTITY,
+		MODEXP,
+		BN128_ADD,
+		BN128_MUL,
+		BN128_PAIRING,
+		BLAKE2F,
+		SHA3_FIPS256,
+		EC_RECOVER_PUBLIC_KEY,
+		ED25519_VERIFY,
+		SR25519_VERIFY,
+		ECDSA_VERIFY,
+		XVM,
+	];
 
 	const fn hash(a: u64) -> H160 {
 		let bytes = a.to_be_bytes();
@@ -51,6 +69,18 @@ pub mod consts {
 		result[18] = bytes[6];
 		result[19] = bytes[7];
 		H160(result)
+	}
+
+	#[cfg(test)]
+	mod tests {
+		use super::hash;
+		use sp_core::H160;
+		use test_strategy::proptest;
+
+		#[proptest]
+		fn hash_function_is_correct(a: u64) {
+			assert_eq!(hash(a), H160::from_low_u64_be(a));
+		}
 	}
 }
 
@@ -72,32 +102,16 @@ impl<R> FrontierPrecompiles<R> {
 	/// * 0x9 - is Blake2F
 	///
 	/// The next list contains handy precompiles that are missing in Ethereum
+	/// * 0xF3 - is Ed25519 verify
 	/// * 0x400 - is Sha3
 	/// * 0x402 - is ECRecoverPublicKey (402 is used in Astar and Moonbeam, so preserve the address for contracts interoperability)
-	/// * 0x403 - is Ed25519 verify (403 is also used in Astar, so preserve the address for contracts interoperability)
 	///
 	/// The next list contains Astar specific precompiles:
 	/// * 0x5002 - is Sr25519 verify
 	/// * 0x5003 - is Ecdsa verify
 	/// * 0x5005 - is cross virtual machine (XVM)
 	pub fn used_addresses() -> [H160; 15] {
-		[
-			consts::EC_RECOVER,
-			consts::SHA256,
-			consts::RIPEMD160,
-			consts::IDENTITY,
-			consts::MODEXP,
-			consts::BN128_ADD,
-			consts::BN128_MUL,
-			consts::BN128_PAIRING,
-			consts::BLAKE2F,
-			consts::SHA3_FIPS256,
-			consts::EC_RECOVER_PUBLIC_KEY,
-			consts::ED25519_VERIFY,
-			consts::SR25519_VERIFY,
-			consts::ECDSA_VERIFY,
-			consts::XVM,
-		]
+		consts::SUPPORTED_PRECOMPILES
 	}
 }
 
