25be01de890beddadc24ea74cec3487eddd6040b
dzmitry-lahoda <dzmiry@lahoda.pro>
2023-03-10 13:26:14 +0000

    ultimate shell

--- flake.nix
+++ flake.nix
@@ -27,13 +27,26 @@
               inherit inputs pkgs;
               modules =
                 let
+                  darwin = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
+                    frameworks.Security
+                  ]);
+
                   rust-toolchain =
                     pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-                  cargo-dylint = with pkgs; rustPlatform.buildRustPackage rec {
+                  dylib = {
+                    buildInputs = with pkgs; [ openssl ];
+                    nativeBuildInputs = with pkgs;[ pkg-config ];
+                    doCheck = false;
+                  };
+                  rust-deps = pkgs.makeRustPlatform {
+                    inherit pkgs;
+                    # dylint needs nightly
+                    cargo = pkgs.rust-bin.beta.latest.default;
+                    rustc = pkgs.rust-bin.beta.latest.default;
+                  };
+                  cargo-dylint = with pkgs; rust-deps.buildRustPackage (rec {
                     pname = "cargo-dylint";
                     version = "2.1.5";
-                    nativeBuildInputs = [ pkgs.pkg-config ];
                     src = fetchCrate {
                       inherit pname version;
                       sha256 = "sha256-kH6dhUFaQpQ0kvzNyLIXjFAO8VNa2jah6ZaDO7LQKO0=";
@@ -41,27 +54,23 @@
 
                     cargoHash = "sha256-YvQI3H/4eWe6r2Tg8qHJqfnw/NpuGHtkRuTL4EzF0xo=";
                     cargoDepsName = pname;
-                  };
-                  dylint-link = with pkgs; rustPlatform.buildRustPackage rec {
+                  } // dylib);
+                  dylint-link = with pkgs; rust-deps.buildRustPackage (rec {
                     pname = "dylint-link";
                     version = "2.1.5";
-                    nativeBuildInputs = [ pkgs.pkg-config ];
                     src = fetchCrate {
                       inherit pname version;
                       sha256 = "sha256-oarEYhv0i2wAPmahx0vgWN3kmfEsK3s6D3+qkOqF9pc=";
                     };
 
-                    cargoHash = "sha256-YvQI3H/4eWe6r2Tg8qHJqfnw/NpuGHtk3uTL4EzF1xo=";
+                    cargoHash = "sha256-pMr9hddHAIyIclHRpxqdUaHphjSAVDnvfNjWGDA2EM4=";
                     cargoDepsName = pname;
-                  };
-
+                  } // dylib);
+                  # can `cargo-contract` and nodejs ui easy here 
                 in
                 [
                   {
-                    packages = with pkgs;[ rust-toolchain clang llvmPackages.bintools cargo-dylint dylint-link ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
-                      frameworks.Security
-                    ]);
-
+                    packages = with pkgs;[ rust-toolchain binaryen clang llvmPackages.bintools dylint-link ] ++ darwin;
                     env = with pkgs; {
                       LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath [
                         pkgs.stdenv.cc.cc.lib
@@ -70,7 +79,9 @@
                       LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
                       PROTOC = "${pkgs.protobuf}/bin/protoc";
                       ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
+                      RUSTUP_TOOLCHAIN = "nightly-2022-12-20"; # could read from toml for dylint
                     };
+                    # can do systemd/docker stuff here
                     enterShell = ''
                       echo ggshell
                     '';
