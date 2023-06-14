f77b70817ab9adef12e3955e79f48dd2542cd7fc
akorchyn <artur.yurii.korchynskyi@ggxchain.io>
2023-06-14 14:22:25 +0300

    Reverted unintentional change

--- node/src/runtime/mainnet.rs
+++ node/src/runtime/mainnet.rs
@@ -155,7 +155,7 @@ pub fn testnet_genesis(
 		runtime_specification: RuntimeSpecificationConfig {
 			chain_spec: RuntimeConfig {
 				block_time_in_millis: 2000,
-				session_time_in_seconds: 8, // 4 hours
+				session_time_in_seconds: 4 * 3600, // 4 hours
 			},
 		},
 		vesting: Default::default(),
