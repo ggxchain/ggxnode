3960f26f3502c82aac8ba1403c134771dce59c1b
akorchyn <artur.yurii.korchynskyi@ggxchain.io>
2023-06-21 13:59:43 +0300

    removed permissioned ledger

--- runtime/mainnet/src/version.rs
+++ runtime/mainnet/src/version.rs
@@ -7,7 +7,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("golden-gate-node"),
 	impl_name: create_runtime_str!("golden-gate-node"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
