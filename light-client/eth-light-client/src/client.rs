967524e76bd7b13fc1f58d5441a85812b9409a1e
Obsessed Cake <aaaoxxx@gmail.com>
2023-07-11 06:59:58 +0300

    Formatting

--- light-client/eth-light-client/src/client.rs
+++ light-client/eth-light-client/src/client.rs
@@ -1,68 +1,67 @@
 use std::{
-    path::PathBuf,
-    process::exit,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
+	path::PathBuf,
+	process::exit,
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		Arc,
+	},
 };
 
 use ethers::types::Address;
 use eyre::Result;
 use helios::{client::ClientBuilder, config::networks::Network, prelude::*};
 
-use crate::config::Config;
-use crate::db::DB;
+use crate::{config::Config, db::DB};
 
 pub async fn start_client(config: Config, db: DB, term: Arc<AtomicBool>) -> Result<()> {
-    let mut client: Client<FileDB> = ClientBuilder::new()
-        .network(Network::MAINNET)
-        .consensus_rpc(&config.consensus_rpc)
-        .execution_rpc(&config.untrusted_rpc)
-        .load_external_fallback()
-        .data_dir(PathBuf::from(
-            config.helios_home_path.unwrap_or("/tmp/helios".to_string()),
-        ))
-        .build()?;
+	let mut client: Client<FileDB> = ClientBuilder::new()
+		.network(Network::MAINNET)
+		.consensus_rpc(&config.consensus_rpc)
+		.execution_rpc(&config.untrusted_rpc)
+		.load_external_fallback()
+		.data_dir(PathBuf::from(
+			config.helios_home_path.unwrap_or("/tmp/helios".to_string()),
+		))
+		.build()?;
 
-    log::info!(
-        "Built client on network \"{}\" with external checkpoint fallbacks",
-        Network::MAINNET
-    );
+	log::info!(
+		"Built client on network \"{}\" with external checkpoint fallbacks",
+		Network::MAINNET
+	);
 
-    exit_if_term(term.clone());
+	exit_if_term(term.clone());
 
-    client.start().await?;
-    log::info!("client started");
+	client.start().await?;
+	log::info!("client started");
 
-    let filter = ethers::types::Filter::new()
-        .select(
-            config
-                .block_number
-                .map(Into::into)
-                .unwrap_or(ethers::core::types::BlockNumber::Latest)..,
-        )
-        .address(config.smart_contract_address.parse::<Address>()?)
-        .event("Transfer(address,address,uint256)");
+	let filter = ethers::types::Filter::new()
+		.select(
+			config
+				.block_number
+				.map(Into::into)
+				.unwrap_or(ethers::core::types::BlockNumber::Latest)..,
+		)
+		.address(config.smart_contract_address.parse::<Address>()?)
+		.event("Transfer(address,address,uint256)");
 
-    loop {
-        exit_if_term(term.clone());
-        let logs = client.get_logs(&filter).await?;
-        log::info!("logs: {:#?}", logs);
-        for log in logs {
-            if let Some(block_number) = log.block_number {
-                if let Some(log_index) = log.log_index {
-                    let json = serde_json::to_string(&log)?;
-                    db.insert_logs(block_number.low_u64(), log_index.low_u64(), &json)?;
-                }
-            }
-        }
-    }
+	loop {
+		exit_if_term(term.clone());
+		let logs = client.get_logs(&filter).await?;
+		log::info!("logs: {:#?}", logs);
+		for log in logs {
+			if let Some(block_number) = log.block_number {
+				if let Some(log_index) = log.log_index {
+					let json = serde_json::to_string(&log)?;
+					db.insert_logs(block_number.low_u64(), log_index.low_u64(), &json)?;
+				}
+			}
+		}
+	}
 }
 
 fn exit_if_term(term: Arc<AtomicBool>) {
-    if term.load(Ordering::Relaxed) {
-        log::info!("caught SIGTERM");
-        exit(0);
-    }
+	if term.load(Ordering::Relaxed) {
+		log::info!("caught SIGTERM");
+		exit(0);
+	}
 }
--- light-client/eth-light-client/src/main.rs
+++ light-client/eth-light-client/src/main.rs
@@ -15,28 +15,28 @@ use server::start_server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
+	env_logger::init();
 
-    let config = envy::from_env::<Config>()?;
-    let term = Arc::new(AtomicBool::new(false));
-    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
+	let config = envy::from_env::<Config>()?;
+	let term = Arc::new(AtomicBool::new(false));
+	signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
 
-    let db = DB::new(&config)?;
-    db.create_table()?;
+	let db = DB::new(&config)?;
+	db.create_table()?;
 
-    let clone_db = db.clone();
-    let clone_config = config.clone();
-    tokio::spawn(async move {
-        let res = start_server(clone_config, clone_db).await;
-        log::info!("server was stopped, reason: {:?}", res);
-    });
+	let clone_db = db.clone();
+	let clone_config = config.clone();
+	tokio::spawn(async move {
+		let res = start_server(clone_config, clone_db).await;
+		log::info!("server was stopped, reason: {:?}", res);
+	});
 
-    tokio::spawn(async move {
-        let res = start_client(config, db.clone(), term).await;
-        log::info!("client was stopped, reason: {:?}", res);
-    })
-    .await
-    .expect("stop client successfully");
+	tokio::spawn(async move {
+		let res = start_client(config, db.clone(), term).await;
+		log::info!("client was stopped, reason: {:?}", res);
+	})
+	.await
+	.expect("stop client successfully");
 
-    Ok(())
+	Ok(())
 }
