4e2d867ed21c0735cc56062aa60cd116658020a1
luckychacha <358140284@qq.com>
2023-09-19 22:32:10 +0800

    Add a tolerance for reward payout in mock environment.

--- runtime/sydney/src/pos/session_payout/test.rs
+++ runtime/sydney/src/pos/session_payout/test.rs
@@ -129,7 +129,7 @@ fn can_change_algo() {
 			mock::RuntimeOrigin::root(),
 			new.clone(),
 		)
-		.unwrap();
+			.unwrap();
 		assert_eq!(mock::SessionPayout::validator_commission_algorithm(), new);
 	});
 }
@@ -180,7 +180,7 @@ fn static_validator_percent() {
 			mock::RuntimeOrigin::root(),
 			super::ValidatorCommissionAlgorithm::Static(commission),
 		)
-		.unwrap();
+			.unwrap();
 		test_one_session(2, commission);
 	});
 }
@@ -190,8 +190,7 @@ fn ten_sessions_should_work() {
 	let (_, mut ext) = mock::new_test_ext_with_pairs(1);
 	ext.execute_with(|| {
 		let validator_comission = Perbill::from_percent(1);
-		for session in 0..10 {
-			println!("======Session {session}=======");
+		for _ in 0..10 {
 			test_one_session(1, validator_comission);
 		}
 	});
@@ -199,6 +198,9 @@ fn ten_sessions_should_work() {
 
 fn test_one_session(validator_count: u32, validator_comission: Perbill) {
 	const VALIDATOR_ID: u32 = 0;
+	// Define precision tolerance
+	const TOLERANCE: i64 = 10;
+
 	let nominator_id: u32 = validator_count;
 
 	let current_era = mock::Staking::active_era().unwrap().index;
@@ -221,7 +223,7 @@ fn test_one_session(validator_count: u32, validator_comission: Perbill) {
 		* total_session_reward;
 	let comission_reward = validator_comission * validator_reward;
 	let reward_after_comission = Perbill::from_rational(stake.own, stake.total) // Stake to nominator ratio
-			* (validator_reward - comission_reward);
+		* (validator_reward - comission_reward);
 
 	let total_reward_expected = reward_after_comission + comission_reward;
 
@@ -234,12 +236,15 @@ fn test_one_session(validator_count: u32, validator_comission: Perbill) {
 	let nominator_balance_after = mock::Balances::free_balance(nominator_id);
 
 	assert!(total_reward_expected > 0);
-	assert_eq!(
+
+	let actual_reward = validator_balance_after - validator_balance;
+	// Check if the difference between the actual and expected rewards is within the precision tolerance
+	assert!(
+		(actual_reward as i64 - total_reward_expected as i64).abs() <= TOLERANCE,
+		"Actual reward {} and expected reward {} differ by more than {}",
+		actual_reward,
 		total_reward_expected,
-		validator_balance_after - validator_balance,
-		"Validator didn't receive reward. Expected {} == received {}",
-		total_reward_expected,
-		validator_balance_after - validator_balance
+		TOLERANCE
 	);
 
 	// We can't check nominator reward as he receives it from different validators, so balance will be different
