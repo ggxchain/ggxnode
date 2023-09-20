686f6cfc4e51f4e72f0c27ed410a2ce09a7d1be3
Smith Li <jinrishouji@sina.com>
2023-09-20 11:13:52 +0800

    remove unuse import

--- node/src/service/brooklyn.rs
+++ node/src/service/brooklyn.rs
@@ -18,8 +18,6 @@ use sc_client_api::{
 	backend::{Backend, StateBackend},
 	AuxStore, BlockchainEvents, StorageProvider,
 };
-use sc_executor::NativeElseWasmExecutor;
-use sc_keystore::LocalKeystore;
 use sc_network::NetworkService;
 use sc_network_sync::SyncingService;
 use sc_rpc::SubscriptionTaskExecutor;
@@ -99,7 +97,7 @@ pub fn new_partial(
 		})
 		.transpose()?;
 
-	let executor = sc_service::new_native_or_wasm_executor(&config);
+	let executor = sc_service::new_native_or_wasm_executor(config);
 
 	let (client, backend, keystore_container, task_manager) =
 		sc_service::new_full_parts::<Block, RuntimeApi, _>(
