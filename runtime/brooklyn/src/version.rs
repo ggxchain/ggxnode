9b5633e2642b9cc5964354c84549d540eca077d1
Artur Yurii Korchynskyi <42449190+akorchyn@users.noreply.github.com>
2023-12-13 17:14:17 +0200

    sepolia (#220)

--- runtime/brooklyn/src/version.rs
+++ runtime/brooklyn/src/version.rs
@@ -7,7 +7,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("ggxchain-node"),
 	impl_name: create_runtime_str!("ggxchain-node"),
 	authoring_version: 1,
-	spec_version: 9,
+	spec_version: 10,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
