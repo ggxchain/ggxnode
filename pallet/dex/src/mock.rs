df36ae9adb2dc1a70a056ba4bba099ceab1151df
Li Smith <jinrishouji@sina.com>
2024-05-22 21:17:02 +0800

    update dex mock

--- pallet/dex/src/mock.rs
+++ pallet/dex/src/mock.rs
@@ -1,16 +1,28 @@
 use crate as pallet_dex;
 
+use super::*;
+
 use frame_support::{
 	pallet_prelude::Weight,
 	parameter_types, sp_io,
-	traits::{AsEnsureOriginWithArg, GenesisBuild, Hooks},
+	traits::{AsEnsureOriginWithArg, GenesisBuild, Hooks, Nothing},
 	weights::constants::RocksDbWeight,
 	PalletId,
 };
-use sp_core::{ConstU128, ConstU32, ConstU64, H256};
-use sp_runtime::{testing::Header, traits::IdentityLookup};
+use ggx_primitives::currency::{CurrencyId, TokenSymbol};
+use orml_tokens::TransferDust;
+use orml_traits::{currency::MutationHooks, parameter_type_with_key};
+use sp_core::{ConstU128, ConstU32, ConstU64, H160, H256};
+use sp_runtime::{
+	testing::Header,
+	traits::{AccountIdConversion, IdentityLookup},
+	AccountId32,
+};
+use sp_std::marker;
 
-pub type AccountId = u128;
+use pallet_currencies::{BasicCurrencyAdapter, NativeCurrencyOf};
+
+pub type AccountId = AccountId32;
 pub type Balance = u128;
 pub type AssetId = u32;
 pub type BlockNumber = u64;
@@ -27,8 +39,10 @@ frame_support::construct_runtime!(
 		System: frame_system,
 		Balances: pallet_balances,
 		Timestamp: pallet_timestamp,
-	Assets: pallet_assets,
-	Dex: pallet_dex,
+		Currencies: pallet_currencies,
+		Tokens: orml_tokens,
+		Assets: pallet_assets,
+		Dex: pallet_dex,
 	}
 );
 
@@ -132,6 +146,104 @@ impl pallet_timestamp::Config for Test {
 	type WeightInfo = ();
 }
 
+parameter_type_with_key! {
+	pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+		Default::default()
+	};
+}
+
+parameter_types! {
+	pub DustAccount: AccountId = PalletId(*b"orml/dst").into_account_truncating();
+}
+
+pub struct CurrencyHooks<T>(marker::PhantomData<T>);
+impl<T: orml_tokens::Config> MutationHooks<T::AccountId, T::CurrencyId, T::Balance>
+	for CurrencyHooks<T>
+where
+	T::AccountId: From<AccountId>,
+{
+	type OnDust = orml_tokens::TransferDust<T, DustAccount>;
+	type OnSlash = ();
+	type PreDeposit = ();
+	type PostDeposit = ();
+	type PreTransfer = ();
+	type PostTransfer = ();
+	type OnNewTokenAccount = ();
+	type OnKilledTokenAccount = ();
+}
+
+pub type ReserveIdentifier = [u8; 8];
+
+impl orml_tokens::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type Balance = Balance;
+	type Amount = i64;
+	type CurrencyId = CurrencyId;
+	type WeightInfo = ();
+	type ExistentialDeposits = ExistentialDeposits;
+	type CurrencyHooks = CurrencyHooks<Test>;
+	type MaxLocks = ConstU32<100_000>;
+	type MaxReserves = ConstU32<100_000>;
+	type ReserveIdentifier = ReserveIdentifier;
+	type DustRemovalWhitelist = Nothing;
+}
+
+pub type NativeCurrency = NativeCurrencyOf<Test>;
+pub type AdaptedBasicCurrency = BasicCurrencyAdapter<Test, Balances, i64, u64>;
+
+/// A mapping between `AccountId` and `EvmAddress`.
+pub trait AddressMapping<AccountId> {
+	/// Returns the AccountId used go generate the given EvmAddress.
+	fn get_account_id(evm: &EvmAddress) -> AccountId;
+	/// Returns the EvmAddress associated with a given AccountId or the
+	/// underlying EvmAddress of the AccountId.
+	/// Returns None if there is no EvmAddress associated with the AccountId
+	/// and there is no underlying EvmAddress in the AccountId.
+	fn get_evm_address(account_id: &AccountId) -> Option<EvmAddress>;
+	/// Returns the EVM address associated with an account ID and generates an
+	/// account mapping if no association exists.
+	fn get_or_create_evm_address(account_id: &AccountId) -> EvmAddress;
+	/// Returns the default EVM address associated with an account ID.
+	fn get_default_evm_address(account_id: &AccountId) -> EvmAddress;
+	/// Returns true if a given AccountId is associated with a given EvmAddress
+	/// and false if is not.
+	fn is_linked(account_id: &AccountId, evm: &EvmAddress) -> bool;
+}
+
+pub struct MockAddressMapping;
+
+impl AddressMapping<AccountId32> for MockAddressMapping {
+	fn get_account_id(address: &H160) -> AccountId32 {
+		let mut data = [0u8; 32];
+		data[0..4].copy_from_slice(b"evm:");
+		data[4..24].copy_from_slice(&address[..]);
+		AccountId32::from(data)
+	}
+
+	fn get_evm_address(account_id: &AccountId32) -> Option<H160> {
+		let data: [u8; 32] = account_id.clone().into();
+		if data.starts_with(b"evm:") && data.ends_with(&[0u8; 8]) {
+			Some(H160::from_slice(&data[4..24]))
+		} else {
+			None
+		}
+	}
+
+	fn get_default_evm_address(account_id: &AccountId32) -> H160 {
+		let slice: &[u8] = account_id.as_ref();
+		H160::from_slice(&slice[0..20])
+	}
+}
+
+impl pallet_currencies::Config for Test {
+	type MultiCurrency = Tokens;
+	type NativeCurrency = AdaptedBasicCurrency;
+	type GetNativeCurrencyId = NativeCurrency;
+	type WeightInfo = ();
+	type AddressMapping = MockAddressMapping;
+	type EVMBridge = ();
+}
+
 parameter_types! {
 	pub const DexPalletId: PalletId = PalletId(*b"py/sudex");
 	pub const UnsignedPriority: BlockNumber = 1;
@@ -139,8 +251,9 @@ parameter_types! {
 
 impl pallet_dex::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+	type MultiCurrency = Tokens;
+	type NativeCurrency = AdaptedBasicCurrency;
 	type PalletId = DexPalletId;
-	type Fungibles = Assets;
 	type PrivilegedOrigin = frame_system::EnsureRoot<Self::AccountId>;
 	type Currency = Balances;
 	type UnsignedPriority = UnsignedPriority;
@@ -195,8 +308,12 @@ impl ExtBuilder {
 
 		<pallet_dex::GenesisConfig as frame_support::traits::GenesisBuild<Test>>::assimilate_storage(
       &pallet_dex::GenesisConfig {
-        asset_ids: vec![8888, 999, 888, 777],
-        native_asset_id: 8888,
+        asset_ids: vec![
+					CurrencyId::ForeignAsset(8888),
+					CurrencyId::ForeignAsset(999),
+					CurrencyId::ForeignAsset(888),
+					CurrencyId::ForeignAsset(777),],
+        native_asset_id: CurrencyId::Token(TokenSymbol::GGX),
       },
       &mut storage,
   )
